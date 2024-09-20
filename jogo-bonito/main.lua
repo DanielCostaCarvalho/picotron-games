d20c7d1ee3e52977b3c3e39976eca8dbd3bcb41f
Daniel Costa Carvalho <daniel@elleventecnologia.com.br>
2024-09-20 13:57:57 -0300

    feat(futebol): adiciona verificacao de chute

--- jogo-bonito/main.lua
+++ jogo-bonito/main.lua
@@ -8,6 +8,9 @@ game = {
 ball = {
   x = 240,
   y = 240,
+  hitbox_x = 238,
+  hitbox_y = 238,
+  hitbox_size = 4,
   speed = 5,
   size = 2,
 }
@@ -17,26 +20,51 @@ function ball:draw()
   circ(self.x, self.y, self.size, 6)
 end
 
+function ball:move(x, y, z)
+  self.x += x
+  self.y += y
+  self.hitbox_x += x
+  self.hitbox_y += y
+end
+
 Player = {
   x = 240,
   y = 240,
-  hitbox_size = 9,
-  hitbox_x = 240 + 3,
-  hitbox_y = 240 + 10,
+  shoot_size = 9,
+  shoot_x = 240 + 3,
+  shoot_y = 240 + 10,
+  hitbox_size = 5,
+  hitbox_x = 240 + 5,
+  hitbox_y = 240 + 12,
   speed = 1,
   direction = 0,
   position = 1,
 }
 
+function Player:canShoot()
+  local is_x = false
+  local is_y = false
+
+  if (ball.hitbox_x >= self.shoot_x and ball.hitbox_x <= self.shoot_x + self.shoot_size) or (ball.hitbox_x + ball.hitbox_size >= self.shoot_x and ball.hitbox_x + ball.hitbox_size <= self.shoot_x + self.shoot_size) then
+    is_x = true
+  end
+
+  if (ball.hitbox_y >= self.shoot_y and ball.hitbox_y <= self.shoot_y + self.shoot_size) or (ball.hitbox_y + ball.hitbox_size >= self.shoot_y and ball.hitbox_y + ball.hitbox_size <= self.shoot_y + self.shoot_size) then
+    is_y = true
+  end
+
+  return is_x and is_y
+end
+
 function Player:hasBall()
   local is_x = false
   local is_y = false
 
-  if (ball.x - ball.size >= self.hitbox_x and ball.x - ball.size <= self.hitbox_x + self.hitbox_size) or (ball.x + ball.size >= self.hitbox_x and ball.x + ball.size <= self.hitbox_x + self.hitbox_size) then
+  if (ball.hitbox_x >= self.hitbox_x and ball.hitbox_x <= self.hitbox_x + self.hitbox_size) or (ball.hitbox_x + ball.hitbox_size >= self.hitbox_x and ball.hitbox_x + ball.hitbox_size <= self.hitbox_x + self.hitbox_size) then
     is_x = true
   end
 
-  if (ball.y - ball.size >= self.hitbox_y and ball.y - ball.size <= self.hitbox_y + self.hitbox_size) or (ball.y + ball.size >= self.hitbox_y and ball.y + ball.size <= self.hitbox_y + self.hitbox_size) then
+  if (ball.hitbox_y >= self.hitbox_y and ball.hitbox_y <= self.hitbox_y + self.hitbox_size) or (ball.hitbox_y + ball.hitbox_size >= self.hitbox_y and ball.hitbox_y + ball.hitbox_size <= self.hitbox_y + self.hitbox_size) then
     is_y = true
   end
 
@@ -81,14 +109,19 @@ function Player:move()
     self.position = 1
   end
 
+  if btn(5) and self:canShoot() then
+    ball:move(x * 10, y * 10, 0)
+  end
+
   self.y += y
   self.x += x
   self.hitbox_y += y
   self.hitbox_x += x
+  self.shoot_y += y
+  self.shoot_x += x
 
   if self:hasBall() then
-    ball.x += x
-    ball.y += y
+    ball:move(x, y, 0)
   end
 end
 
@@ -101,6 +134,7 @@ function Player:draw()
   }
   sprite_info = sprites[self.direction + 1][flr(self.position)]
   rect(self.hitbox_x, self.hitbox_y, self.hitbox_x + self.hitbox_size, self.hitbox_y + self.hitbox_size, 10)
+  rect(self.shoot_x, self.shoot_y, self.shoot_x + self.shoot_size, self.shoot_y + self.shoot_size, 2)
   spr(sprite_info[1], self.x, self.y, sprite_info[2])
 end
 
